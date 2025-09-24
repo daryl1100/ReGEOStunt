5f40d4ee7841f25a2e9b0e3504141a84bfb898e3
daryl1100 <237006061@student.unsil.ac.id>
2025-09-25 02:14:11 +0700

    Update app1.py

--- app1.py
+++ app1.py
@@ -1103,6 +1103,9 @@ st.markdown("""
     .menu a:hover {
         background-color: #f8f9fa;
     }
+
+    .stSelectbox [class*="material-icons"] {
+        display: none !important;
 </style>
 """, unsafe_allow_html=True)
 
@@ -1470,3 +1473,4 @@ st.markdown("""
     <p style="margin-top: 1rem; font-size: 0.9rem; color: #94a3b8;">Versi 1.0 | Terakhir diperbarui: Juni 2024</p>
 </div>
 """, unsafe_allow_html=True)
+
